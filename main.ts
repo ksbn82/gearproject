8ac2679fe0840052392e18ee7ba68eae1eadbefc
ksbn82 <35519990+ksbn82@users.noreply.github.com>
2021-04-13 20:37:51 +0900

    pxt.json, main.blocks, main.ts, Gearproject.json 업데이트하기

--- main.ts
+++ main.ts
@@ -1,8 +1,12 @@
-basic.forever(function () {
+function URFlednums (Number2: number) {
     for (let index = 0; index <= 4; index++) {
         for (let index2 = 0; index2 <= 4; index2++) {
-            led.toggle(index2, index)
-            basic.pause(50)
+            led.unplot(index2, index)
         }
     }
-})
+    if (Number2 == 0) {
+        let index = 0
+        let index2 = 0
+        led.unplot(index2, index)
+    }
+}
